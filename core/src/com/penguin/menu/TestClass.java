64cd88c586da8a5b238a067186163c157b87c6aa
TonyForge <tonyforge.mail@gmail.com>
2015-12-19 22:47:57 +0300

    Просто тестовый класс, проверить как работает коммит.

---
+++ core/src/com/penguin/menu/TestClass.java
@@ -0,0 +1,12 @@
+package com.penguin.menu;
+
+import com.mypinguin.game.PenguinGame;
+
+public class TestClass extends ExtendedScreen {
+
+	public TestClass(PenguinGame penguinGame) {
+		super(penguinGame);
+		// TODO Auto-generated constructor stub
+	}
+
+}
