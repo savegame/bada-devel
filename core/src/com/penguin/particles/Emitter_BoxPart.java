9cbd41b857935cc8d7f3d50ee3c3d075cda31c79
savegame <codecodition@gmail.com>
2016-05-23 22:55:55 +0300

    сделал простой и тупой разлет частиц сломанной коробки

--- core/src/com/penguin/particles/Emitter_BoxPart.java
+++ core/src/com/penguin/particles/Emitter_BoxPart.java
@@ -1,6 +1,7 @@
 package com.penguin.particles;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.mypinguin.game.PenguinGame;
 
 /**
@@ -9,6 +10,10 @@ import com.mypinguin.game.PenguinGame;
 public class Emitter_BoxPart extends BaseEmitter<Particle_BoxPart> {
 
 	private float emissionTimer = 0;
+	private float angleIncrement = 360f / 10f; //часть угла
+	private int anglePart = 0; //текущая часть
+
+
 
 	public Emitter_BoxPart(PenguinGame game, Sprite sprite, Class<Particle_BoxPart> particleClass) {
 		super(game, sprite, particleClass);
@@ -35,6 +40,8 @@ public class Emitter_BoxPart extends BaseEmitter<Particle_BoxPart> {
 						getY() + (game.rand.nextFloat()-0.5f)*game.units*0.5f);
 		particle.life = 10.0f;
 		particle.setScale(game.rand.nextFloat()*3);
+		particle.setRotation( angleIncrement*anglePart + game.rand.nextFloat()*angleIncrement*0.33f );
+		anglePart++;
 	}
 
 	public boolean updateParticle(float delta, Particle particle) {
@@ -42,7 +49,14 @@ public class Emitter_BoxPart extends BaseEmitter<Particle_BoxPart> {
 		Particle_BoxPart part = (Particle_BoxPart)particle;
 		part.life -= delta;
 		if( part.life <= 0 ) return false;
-
+		float x = part.getX() + part.getVelocity()* MathUtils.sinDeg(part.getRotation());
+		float y = part.getY() + part.getVelocity()* MathUtils.cosDeg(part.getRotation());
+		part.setPosition(x,y);
 		return true;
+	}
+
+	public void generate(int particlesCount) {
+		super.generate(particlesCount);
+		angleIncrement = 360f / particlesCount;
 	}
 }
--- core/src/com/penguin/particles/Particle_BoxPart.java
+++ core/src/com/penguin/particles/Particle_BoxPart.java
@@ -9,6 +9,8 @@ public class Particle_BoxPart extends Particle {
 
 	float horizontal_speed = 0.5f;
 	float screenScale;
+	float angle = 0f; //угол полета
+	private float velocity = 5f;
 //	boolean solid;
 	//float translation_x, translation_y;
 
@@ -17,5 +19,13 @@ public class Particle_BoxPart extends Particle {
 		//solid = false;
 		//translation_x = 0;
 		//translation_y = 0;
+	}
+
+	public float getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(float vel) {
+		velocity = vel;
 	}
 }
